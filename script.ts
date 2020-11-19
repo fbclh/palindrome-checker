ae0b2c070d7a03c05bee656b93efd41fa45772b6
fbclh <fbclh@protonmail.com>
2020-11-19 18:56:16 -0300

    Add script.ts and delete script.js

--- script.ts
+++ script.ts
@@ -1,8 +1,8 @@
 //  Palindrome checker
 //  Return true if the given string is a palindrome. Otherwise, return false.
-//  A palindrome is a word or sentence that's spelled the same way both forward 
+//  A palindrome is a word or sentence that's spelled the same way both forward
 // and backward, ignoring punctuation, case, and spacing.
- 
+
 function palindrome(str) {
   if (
     str

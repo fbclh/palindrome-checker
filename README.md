13e68d57ff1b15dde4b99bcfbe18efb98b607df1
fbclh <fbclh@protonmail.com>
2020-11-28 09:26:04 -0300

    Refactor algo and edit README

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # Palindrome Checker
 
 freeCodeCamp JavaScript Algorithms and Data Structure project
+## Description
 
-## To Do
+Return ```true``` if the given string is a palindrome. Otherwise, return ```false```.
 
-1. Make a UI version
-1. Add screenshot
+A palindrome is a word or sentence that's spelled the same way both forward and backward, 
+ignoring punctuation, case, and spacing.
+
+## Example
+
+```JavaScript
+//  Palindrome Checker
+const palindrome = (str) => {
+  return str
+    .split('')
+    .reverse()
+    .join('')
+    .toLowerCase()
+    .replace(/[^0-9a-zA-Z]/g, '') ===
+    str.toLowerCase().replace(/[^0-9a-zA-Z]/g, '')
+    ? true
+    : false;
+};
+
+// Input/Output 
+console.log(palindrome('eye')); // true
+console.log(palindrome('toyota')); // false
+console.log(palindrome('spinach')); // false
+```
--- script.ts
+++ script.ts
@@ -1,26 +1,17 @@
-//  Palindrome checker
-//  Return true if the given string is a palindrome. Otherwise, return false.
-//  A palindrome is a word or sentence that's spelled the same way both forward
-// and backward, ignoring punctuation, case, and spacing.
-
-function palindrome(str) {
-  if (
-    str
-      .split('')
-      .reverse()
-      .join('')
-      .toLowerCase()
-      .replace(/[^0-9a-zA-Z]/g, '') ===
+//  Palindrome Checker
+const palindrome = (str) => {
+  return str
+    .split('')
+    .reverse()
+    .join('')
+    .toLowerCase()
+    .replace(/[^0-9a-zA-Z]/g, '') ===
     str.toLowerCase().replace(/[^0-9a-zA-Z]/g, '')
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
+    ? true
+    : false;
+};
 
+// Input/Output 
 console.log(palindrome('eye')); // true
-console.log(palindrome('race car')); // true
 console.log(palindrome('toyota')); // false
 console.log(palindrome('spinach')); // false
-console.log(palindrome('0_0 (: /- :) 0-0')); // true
